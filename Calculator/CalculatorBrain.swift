c2ab036f5044727c49b7e425c6735c533d98a552
avi <avinashsajjan@gmail.com>
2015-02-09 14:44:10 +0530

    enum Op conforms to Printable protocol and pretty prints!

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,10 +10,20 @@ import Foundation
 
 class CalculatorBrain {
     
-    private enum Op {
+    private enum Op: Printable {
         case Operand(Double)
         case UnaryOperation(String, Double -> Double)
         case BinaryOperation(String, (Double, Double) -> Double)
+
+        var description: String {
+            get {
+                switch self {
+                    case .Operand(let operand): return "\(operand)"
+                    case .UnaryOperation(let symbol, _): return symbol
+                    case .BinaryOperation(let symbol, _): return symbol
+                }
+            }
+        }
     }
     // Alternatives:
     // var opStack: Array<Op> = Array<Op>()
@@ -47,6 +57,7 @@ class CalculatorBrain {
     
     func evaluate() -> Double? {
         let (result, remainder) = evaluate(opStack)
+        println("\(opStack) = \(result) with \(remainder) left over")
         return result
     }
     
